5b7b3b1c63bdfb8a332634408627a6e7ff74f76c
Oliver Szvetnyik <oliver.szvetnyik@gmail.com>
2024-10-22 17:59:23 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'^/api': {
-				target: 'http://localhost:8080',
+				target: 'https://libri-backend.fly.dev',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
 			}
