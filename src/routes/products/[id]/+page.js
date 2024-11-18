f8dbbaa0f6c501e3e9ececce803a8b73c9c1f6df
Oliver Szvetnyik <oliver.szvetnyik@gmail.com>
2024-11-18 17:45:01 +0100

    Fix reset password

--- src/routes/products/[id]/+page.js
+++ src/routes/products/[id]/+page.js
@@ -1,5 +1,4 @@
 export const prerender = false;
-
 export async function load({ params, fetch }) {
 	const productId = params.id;
 	const bookData = await fetch(`/api/books/${productId}`).then((data) => data.json());
