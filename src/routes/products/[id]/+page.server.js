651b5815a33dace46778d86eaebc2581c6b75a8b
Oliver Szvetnyik <oliver.szvetnyik@gmail.com>
2024-11-18 17:26:00 +0100

    Rework /products/[id] server side load

--- src/routes/products/[id]/+page.server.js
+++ src/routes/products/[id]/+page.server.js
@@ -1,6 +1,12 @@
 export async function load({ params, fetch }) {
 	const productId = params.id;
-	const bookData = await fetch(`/api/books/${productId}`).then((data) => data.json());
+	const response = await fetch(`/api/books/${productId}`);
+	if (!response.ok) {
+		const errorText = await response.text(); // Get the response as text
+		console.error('Error fetching data:', errorText); // Log the error
+		throw new Error('Failed to fetch data');
+	}
+	const bookData = await response.json();
 
 	return {
 		bookData
