2087b6a54092947ea6827faee3798b2b5892476b
Oliver Szvetnyik <oliver.szvetnyik@gmail.com>
2024-12-12 17:58:25 +0100

    Change /books/ routes

--- src/routes/products/[id]/+page.js
+++ src/routes/products/[id]/+page.js
@@ -2,7 +2,7 @@ export const prerender = false;
 
 export async function load({ params, fetch }) {
 	const productId = params.id;
-	const bookData = await fetch(`/api/books/${productId}`).then((data) => data.json());
+	const bookData = await fetch(`/api/book/${productId}`).then((data) => data.json());
 
 	return {
 		bookData
