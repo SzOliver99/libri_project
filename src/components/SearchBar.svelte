7969b7b55abe182ce74d18695fa84bc93538039b
Balazs <molnarbalazs1226@gmail.com>
2024-12-18 17:30:32 +0100

    Add animation to SearchBar

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { X } from 'lucide-svelte';
+	import { fade } from 'svelte/transition';
 	import { createEventDispatcher, onMount } from 'svelte';
 	const dispatch = createEventDispatcher();
 
@@ -37,20 +38,22 @@
 		onkeyup={searchHandle}
 		onfocus={handleFocus}
 		onblur={handleBlur}
-		class="mt-1 block w-full rounded-md border border-gray-300 px-3 py-3 pl-6 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 md:text-sm transition-colors duration-300"
+		class="mt-1 block w-full rounded-md border border-gray-300 px-3 py-3 pl-6 shadow-sm transition-colors duration-300 focus:border-primary-500 focus:outline-none focus:ring-primary-500 md:text-sm"
 		placeholder="Search products or authors..."
 		oninput={(e) => (filteredList = e.target.value ? filteredList : [])}
 	/>
 	{#if showIcon}
-		<X
-			class="absolute right-4 top-[1.8rem] -translate-y-1/2 cursor-pointer transition-all duration-300 md:top-[1.7rem] {isFocused
-				? 'text-primary-500'
-				: 'text-gray-300'}"
-			onclick={() => {
-				input.value = '';
-				showIcon = false;
-			}}
-		/>
+		<div transition:fade={{ duration: 100 }}>
+			<X
+				class="absolute right-4 top-[1.8rem] -translate-y-1/2 cursor-pointer transition-all duration-300 md:top-[1.7rem] {isFocused
+					? 'text-primary-500'
+					: 'text-gray-300'}"
+				onclick={() => {
+					input.value = '';
+					showIcon = false;
+				}}
+			/>
+		</div>
 	{/if}
 </div>
 
