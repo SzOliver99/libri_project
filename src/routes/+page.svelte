fdcc05e549d3c45df6e776438fbb5454feddcb0c
Balazs <molnarbalazs1226@gmail.com>
2025-01-27 16:06:21 +0100

    Create Home/About page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,67 @@
 <script>
+	import { ChevronLeft, ChevronRight } from 'lucide-svelte';
+	import { onMount } from 'svelte';
+
+	let currentIndex = 0;
+	const books = [
+		{
+			image: 'https://www.libri.hu/images/upload/banner/kekesteto_1216x480_360x440prima_.png'
+		},
+		{
+			image: 'https://www.libri.hu/images/upload/banner/a_vedelmezo_1216x480_360x440prim.png'
+		},
+		{
+			image: 'https://www.libri.hu/images/upload/banner/het_negyzetmeter_1216x480_360x44.png'
+		}
+	];
+
+	const nextSlide = () => {
+		currentIndex = (currentIndex + 1) % books.length;
+	};
+
+	const prevSlide = () => {
+		currentIndex = (currentIndex - 1 + books.length) % books.length;
+	};
+
+	onMount(() => {
+		setInterval(() => {
+			currentIndex = (currentIndex + 1) % books.length;
+		}, 5000);
+	});
 </script>
 
 <svelte:head>
 	<title>Home Page</title>
 </svelte:head>
 
-<section>Main Page</section>
+<section class="container mx-auto px-4 py-10">
+	<h1 class="mb-8 text-center text-4xl font-bold text-primary-500">Welcome to Library Basement</h1>
+
+	<div class="relative w-full overflow-hidden">
+		<div
+			class="flex transition-transform duration-500 ease-in-out"
+			style="transform: translateX(-{currentIndex * 100}%);"
+		>
+			{#each books as book}
+				<div class="min-w-full">
+					<div class="flex h-full flex-col items-center rounded-lg bg-primary-100 text-center">
+						<img src={book.image} alt="" />
+					</div>
+				</div>
+			{/each}
+		</div>
+
+		<button
+			on:click={prevSlide}
+			class="absolute left-4 top-1/2 -translate-y-1/2 transform p-2 text-white transition-all duration-300 hover:text-gray-200"
+		>
+			<ChevronLeft strokeWidth={2} class="size-10" />
+		</button>
+		<button
+			on:click={nextSlide}
+			class="absolute right-4 top-1/2 -translate-y-1/2 transform p-2 text-white transition-all duration-300 hover:text-gray-200"
+		>
+			<ChevronRight strokeWidth={2} class="size-10" />
+		</button>
+	</div>
+</section>
